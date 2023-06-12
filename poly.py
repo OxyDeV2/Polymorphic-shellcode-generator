e1a28f2da175659586576f9bd4d649f6969c550d
Alexy "OxyDe <46215222+OxyDeV2@users.noreply.github.com>
2023-06-12 12:38:55 +0200

    Update poly.py

--- poly.py
+++ poly.py
@@ -6,11 +6,14 @@ import sys
 
 # - Ma fonction main - #
 def poly(arg1, arg2):
+
     # Code de la fonction ici #
     print("Arguments reçus :", arg1, arg2)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
+        print("\n")
+        print("## - Générateur de shellcode - ##\n")
         print("Le programme nécessite 2 arguments minimum.\n")
         print("python3 poly [shellcode] [chiffrement]")
         print("Exemple :python3 poly fichieravecshellcode.txt xor \n")

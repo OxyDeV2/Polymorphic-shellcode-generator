575370d7083d75c328b55d8f4c6840f086ef01dd
Skriix <83827913+Skriix@users.noreply.github.com>
2023-06-12 15:17:58 +0200

    update

--- poly.py
+++ poly.py
@@ -18,20 +18,6 @@ def help():
 
 # - Ma fonction main - #
 def poly(arg1, arg2):
-    
-monshellcode = b"arg"  # Remplacez par votre propre shellcode
-cle = 0xAA  # Remplacez par votre propre clé
-
-xored_shellcode = bytearray()
-
-for byte in shellcode:
-    xored_byte = byte ^ key
-    xored_shellcode.append(xored_byte)
-
-# Affichage du shellcode XORé
-print(xored_shellcode)
-
-
     print("Arguments reçus :", arg1, arg2)
     if arg2 == "xor":
         print("test")

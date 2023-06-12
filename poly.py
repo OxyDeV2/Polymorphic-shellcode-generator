9f057108e5c7995301a9acb5232c88d95a7787d2
Skriix <83827913+Skriix@users.noreply.github.com>
2023-06-12 15:14:17 +0200

    Modifs poly.py

--- poly.py
+++ poly.py
@@ -1,28 +1,43 @@
 ## - Générateur de shellcode polymorphique - ##
 ## -     Développé par Skriix & OxyDe      - ##
 
-# Les bibliothéques #
-import sys
-from xor.py import xor
+# Les bibliothèques #
+import sys, argparse
+from xor import xor
+
+#Paramètres
+parser = argparse.ArgumentParser(description='[+] Chiffrement shellcode')
+parser.add_argument('-s', '--shellcode', help='Entrez le shellcode')
+parser.add_argument('-t', '--type', help='Le type de chiffrement')
+parser.add_argument('-k', '--key', help='La clé')
+args = parser.parse_args()
+
+def help():
+    parser.print_help()
+    exit(1)
 
 # - Ma fonction main - #
 def poly(arg1, arg2):
     # Code de la fonction ici #
     print("Arguments reçus :", arg1, arg2)
     if arg2 == "xor":
+        print("test")
         #xor(arg1)
     elif arg2 == "aes":
+        print("test")
         #aes(arg1)
 
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 3 and sys.arg2 != "xor" or sys.arg2 != "aes":
+
+    shellcode = args.shellcode
+    key = args.shellcode
+    type = args.shellcode
+
+    if len(sys.argv) != 3 and type != "xor" or type != "aes":
+        help()
         print("Le programme nécessite 2 arguments minimum.\n")
         print("./poly [shellcode] [chiffrement]")
         print("Deux chiffrements possible : xor ou aes")
         print("Exemple : ./poly fichieravecshellcode.txt xor \n")
     else:
-        arg1 = sys.argv[1]
-        arg2 = sys.argv[2]
-        poly(arg1, arg2)
+        poly(key, shellcode)

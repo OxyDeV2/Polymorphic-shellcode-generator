a03b4e0c43fb91f437acf096c38a03b75069427a
Alexy "OxyDe <46215222+OxyDeV2@users.noreply.github.com>
2023-06-12 14:39:46 +0200

    Chiffrement xor
    Need test

---
+++ chiffrement_xor.py
@@ -0,0 +1,15 @@
+
+def xor(monshellcode, cle):
+
+    # Parsing du shellcode str -> bytes ou chaques paires de type 0xAA = un octet
+    shellcodeparsed = bytes.fromhex(monshellcode)
+    xored_monshellcode = bytearray()
+
+    for byte in shellcodeparsed:
+        xored_byte = byte ^ cle
+        xored_monshellcode.append(xored_byte)
+
+    # Affichage du shellcode XOR et original
+    
+    print("Shellcode d'origine :", monshellcode)
+    print("Shellcode chiffré :", xored_monshellcode)

